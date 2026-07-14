1e750c5b4b35901b1f7dea30a64f0f0f3472b013
lucasMunizt <lucasmunizteles@gmail.com>
2026-07-14 09:45:29 -0300

    inicio da tela de agendamentos

--- src/pages/Agendamentos.tsx
+++ src/pages/Agendamentos.tsx
@@ -1,5 +1,30 @@
+import AppShell from "../components/common/app-shell";
+import { Button } from "../components/ui/button";
+
 const Agendamentos = () => {
-  return <div>Agendamentos</div>;
+  return (
+    <div className="bg-[#F9FCFF]">
+      <header className="bg-blue-600 mb-0">
+        <AppShell />
+      </header>
+      <div className="mx-auto max-w-6xl px-4 py-6 sm:px-6 lg:px-8">
+        <div className="flex flex-col justify-between sm:flex-row sm:items-center">
+          <div>
+            <h1 className="text-4xl font-semibold mb-1">Agendamentos</h1>
+            <p className="text-muted-foreground">
+              Gerencie os horários dos seus clientes
+            </p>
+          </div>
+          <div>
+            <Button className="bg-[#0072DA] mt-2 sm:mt-0" size={"lg"}>
+              {" "}
+              + Novo agendamento
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Agendamentos;
