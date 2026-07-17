923bddafcedac0746696169811c3f97ae992cade
lucasMunizt <lucasmunizteles@gmail.com>
2026-07-16 21:02:19 -0300

    add(modalDados) adicionando funções gerais de fortmatação de horas e data

--- src/components/common/modal-dados.tsx
+++ src/components/common/modal-dados.tsx
@@ -5,25 +5,15 @@ import {
   DialogHeader,
   DialogTitle,
 } from "../ui/dialog";
-import { User, Phone, Wrench, Calendar, Clock, FileText } from "lucide-react";
-import { Separator } from "../ui/separator";
+import { User, Phone, Wrench, Calendar, Clock, MapPinned } from "lucide-react";
 import { Row } from "./row";
+import { FormatarData, FormateHoras } from "../../lib/format-date-hours";
+import type { agendamentosTypes } from "../../lib/storage";
 
-type Agendamento = {
-  id: string;
-  cliente: string;
-  telefone: string;
-  servico: string;
-  data: string;
-  hora: string;
-  observacoes?: string;
-  status: "agendado" | "concluido" | "cancelado";
-  createdAt: string;
-};
 interface ModalDadosProps {
   abrir?: boolean;
   setAbrir?: (abrir: boolean) => void;
-  agendamento?: Agendamento | null;
+  agendamento?: agendamentosTypes | null;
 }
 const ModalDados = ({ abrir, setAbrir, agendamento }: ModalDadosProps) => {
   return (
@@ -41,9 +31,8 @@ const ModalDados = ({ abrir, setAbrir, agendamento }: ModalDadosProps) => {
             <div className="flex items-center justify-between">
               <div className="flex items-center gap-2">
                 <User className="h-4 w-4 text-muted-foreground" />
-                <span className="font-medium">{agendamento.cliente}</span>
+                <span className="font-medium">{agendamento.titulo}</span>
               </div>
-              {agendamento.status}
             </div>
 
             {agendamento.telefone && (
@@ -57,23 +46,28 @@ const ModalDados = ({ abrir, setAbrir, agendamento }: ModalDadosProps) => {
             <Row
               icon={<Wrench className="h-4 w-4" />}
               label="Serviço"
-              value={agendamento.servico}
+              value={agendamento.descricao}
             />
 
-            <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-2 gap-40">
               <Row
                 icon={<Calendar className="h-4 w-4" />}
                 label="Data"
-                value={agendamento.data}
+                value={FormatarData(agendamento.data_inicio)}
               />
               <Row
                 icon={<Clock className="h-4 w-4" />}
                 label="Hora"
-                value={agendamento.hora}
+                value={FormateHoras(agendamento.data_inicio)}
               />
             </div>
+            <Row
+              icon={<MapPinned className="h-4 w-4" />}
+              label="Endereço"
+              value={agendamento.endereco}
+            />
 
-            {agendamento.observacoes && (
+            {/* {agendamento.observacoes && (
               <>
                 <Separator />
                 <div className="grid gap-1">
@@ -86,7 +80,7 @@ const ModalDados = ({ abrir, setAbrir, agendamento }: ModalDadosProps) => {
                   </p>
                 </div>
               </>
-            )}
+            )} */}
           </div>
         ) : (
           <p className="py-6 text-center text-sm text-muted-foreground">
