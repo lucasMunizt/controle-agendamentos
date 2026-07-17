d4bb44095329f5613dad96322d3b58eb20d6005c
lucasMunizt <lucasmunizteles@gmail.com>
2026-07-16 21:04:20 -0300

    create(storage) arquivo de tipagem

---
+++ src/lib/storage.ts
@@ -0,0 +1,22 @@
+export type agendamentosTypes = {
+  endereco: string;
+  telefone: string;
+  descricao: string;
+  titulo: string;
+  data_inicio: string;
+  id: string;
+};
+
+
+export type garantiasTypes ={
+  id: string;
+  nome:string;
+  aparelho:string;
+  data: string;
+  pecas:string;
+  valor:number;
+}
+
+
+
+  // o
