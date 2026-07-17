c78c5c9b8c92569fbd8240c167b73dfd74df90eb
lucasMunizt <lucasmunizteles@gmail.com>
2026-07-16 21:05:24 -0300

    alter(agendamentos) alterando pagina para utilizar com dados reais

--- src/pages/Agendamentos.tsx
+++ src/pages/Agendamentos.tsx
@@ -2,7 +2,7 @@ import { Plus, Search } from "lucide-react";
 import AppShell from "../components/common/app-shell";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CardsDadosEspecificos from "../components/common/cards-dados-especificos";
 import {
   Dialog,
@@ -11,150 +11,28 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "../components/ui/dialog";
 
 import { z } from "zod";
 import { Label } from "../components/ui/label";
+import { getAgendamentos } from "../service/GetAgendamentos";
+import type { agendamentosTypes } from "../lib/storage";
+import ModalDados from "../components/common/modal-dados";
 
 const Agendamentos = () => {
   const [query, setQuery] = useState("");
-  const garantias = [
-    {
-      id: 1,
-      os: "001234",
-      data: "2026-07-13",
-      hora: "14:40",
-      nome_cliente: "Maria Oliveira",
-      aparelho: "iPhone 12 Pro",
-      pecas: "Tela original + bateria",
-      valor: 850.0,
-      tipo_garantia: "90 dias",
-      validade_ate: "2026-10-10",
-      na_garantia: true,
-    },
-    {
-      id: 2,
-      os: "001235",
-      data: "2026-07-13",
-      nome_cliente: "João Carlos",
-      aparelho: "Samsung Galaxy S21",
-      pecas: "Conector de carga",
-      valor: 180.0,
-      tipo_garantia: "90 dias",
-      validade_ate: "2026-10-12",
-      na_garantia: true,
-    },
-    {
-      id: 3,
-      os: "001236",
-      data: "2026-07-13",
-      nome_cliente: "Ana Beatriz",
-      aparelho: "Motorola Edge 30",
-      pecas: "Tela frontal",
-      valor: 420.0,
-      tipo_garantia: "90 dias",
-      validade_ate: "2026-10-13",
-      na_garantia: true,
-    },
-    {
-      id: 4,
-      os: "001237",
-      data: "2026-07-16",
-      nome_cliente: "Pedro Henrique",
-      aparelho: "iPhone 11",
-      pecas: "Bateria",
-      valor: 280.0,
-      tipo_garantia: "90 dias",
-      validade_ate: "2026-10-14",
-      na_garantia: true,
-    },
-    {
-      id: 5,
-      os: "001238",
-      data: "2026-07-17",
-      nome_cliente: "Camila Santos",
-      aparelho: "Xiaomi Redmi Note 12",
-      pecas: "Tela + película",
-      valor: 350.0,
-      tipo_garantia: "90 dias",
-      validade_ate: "2026-10-15",
-      na_garantia: true,
-    },
-    {
-      id: 6,
-      os: "001239",
-      data: "2026-07-18",
-      nome_cliente: "Lucas Almeida",
-      aparelho: "Samsung Galaxy A54",
-      pecas: "Tampa traseira",
-      valor: 160.0,
-      tipo_garantia: "90 dias",
-      validade_ate: "2026-10-16",
-      na_garantia: true,
-    },
-    {
-      id: 7,
-      os: "001240",
-      data: "2026-07-19",
-      nome_cliente: "Fernanda Lima",
-      aparelho: "iPhone XR",
-      pecas: "Tela compatível",
-      valor: 390.0,
-      tipo_garantia: "90 dias",
-      validade_ate: "2026-10-17",
-      na_garantia: true,
-    },
-    {
-      id: 8,
-      os: "001241",
-      data: "2026-07-20",
-      nome_cliente: "Rafael Costa",
-      aparelho: "Motorola G60",
-      pecas: "Conector de carga + limpeza interna",
-      valor: 220.0,
-      tipo_garantia: "90 dias",
-      validade_ate: "2026-10-18",
-      na_garantia: true,
-    },
-    {
-      id: 9,
-      os: "001242",
-      data: "2026-07-21",
-      nome_cliente: "Patrícia Gomes",
-      aparelho: "Samsung Galaxy M52",
-      pecas: "Bateria + tampa traseira",
-      valor: 310.0,
-      tipo_garantia: "90 dias",
-      validade_ate: "2026-10-19",
-      na_garantia: true,
-    },
-    {
-      id: 10,
-      os: "001243",
-      data: "2026-07-22",
-      nome_cliente: "Bruno Martins",
-      aparelho: "iPhone 13",
-      pecas: "Tela original",
-      valor: 950.0,
-      tipo_garantia: "90 dias",
-      validade_ate: "2026-10-20",
-      na_garantia: true,
-    },
-    {
-      id: 11,
-      os: "001234",
-      data: "2026-07-13",
-      hora: "14:40",
-      nome_cliente: "Maria Oliveira",
-      aparelho: "iPhone 12 Pro",
-      pecas: "Tela original + bateria",
-      valor: 850.0,
-      tipo_garantia: "90 dias",
-      validade_ate: "2026-10-10",
-      na_garantia: true,
-    },
-  ];
+  const [agendamentos, setAgendamentos] = useState<agendamentosTypes[]>([]);
+  useEffect(() => {
+    async function fetchLotes() {
+      try {
+        const data = await getAgendamentos();
+        setAgendamentos(Array.isArray(data) ? data : []);
+      } catch (error) {
+        console.error("Erro ao mostrar os dados");
+      }
+    }
+    fetchLotes();
+  }, []);
   const aparelhos = [
     "Maquina de lavar Consul",
     "Maquina de lavar Brastemp",
@@ -185,19 +63,22 @@ const Agendamentos = () => {
     status: "agendado",
   };
   type FormState = z.input<typeof schema>;
-
+  const dataAtual = new Date();
   const [form, setForm] = useState<FormState>(empty);
-
+  const filtroAgendamentos = agendamentos.filter((item) => {
+    const dataAgendamento = new Date(item.data_inicio);
+    return dataAgendamento >= dataAtual;
+  });
   return (
     <div className="bg-[#F9FCFF]">
-      <header className="bg-blue-600 mb-0">
+      <header className="mb-0">
         <AppShell />
       </header>
       <div className="mx-auto max-w-6xl px-4 py-6 sm:px-6">
         <div className="flex flex-col justify-between sm:flex-row sm:items-center">
           <div>
             <h1 className="text-4xl font-semibold mb-1">Agendamentos</h1>
-            <p className="text-muted-foreground">
+            <p className="text-muted-foreground text-[12px]">
               Gerencie os horários dos seus clientes
             </p>
           </div>
@@ -309,15 +190,24 @@ const Agendamentos = () => {
           />
         </div>
         <div>
-          <CardsDadosEspecificos
+          <CardsDadosEspecificos<agendamentosTypes>
             titulo="Próximos agendamentos"
             paragrafo="Nenhum agendamento futuro."
-            dados={garantias}
-            rota="/agendamentos"
-            abrir={abrir}
-            setAbrir={setAbrir}
+            dados={filtroAgendamentos}
             valorBusca={query}
-            opcao={false}
+            rota="/agendamentos"
+            opcao={true}
+            getId={(item) => String(item.id)}
+            getTitulo={(item) => item.titulo}
+            getDescricao={(item) => item.descricao}
+            getData={(item) => item.data_inicio}
+            renderModal={(item, abrir, setAbrir) => (
+              <ModalDados
+                abrir={abrir}
+                setAbrir={setAbrir}
+                agendamento={item}
+              />
+            )}
           />
         </div>
       </div>
